85685ee755449937c6b3ed9d8b8b6c58a1174e3c
256hz <a.d@256hz.com>
2019-03-18 15:05:27 -0700

    All methods tested and working

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,63 @@
 class VentureCapitalist
+  attr_accessor :total_worth
+  attr_reader :name
+
+  @@all = [ ]
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.tres_commas_club
+    self.all.select { |vc| vc.total_worth > 1000000000 }
+  end
+
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+  end
+
+  def funding_rounds
+    FundingRound.all.select { |round| round.venture_capitalist == self}
+  end
+
+  def portfolio
+    funding_rounds.map { |round| round.startup }.uniq
+  end
+
+  def biggest_investment
+    funding_rounds.max_by { |round| round.investment}
+  end
+
+  # def biggest_investment
+  #   largest_amount = 0
+  #   largest_round = nil
+  #   funding_rounds.each do |round|
+  #     if round.investment > largest_amount
+  #       largest_round = round
+  #       largest_amount = round.investment
+  #     end
+  #   end
+  #   largest_round
+  # end
+
+  def invested(domain)
+    funding_rounds.inject { |sum, round| sum + round.investment }    
+  end
+
+  # def invested(domain)
+  #   sum = 0
+  #   funding_rounds.each do |round|
+  #     if round.startup.domain == domain
+  #       sum += round.investment
+  #     end
+  #   end
+  #   sum
+  # end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+flatiron = Startup.new("Flatiron School", "Avi", "flatironschool.com")
+microsoft = Startup.new("Microsoft", "Bill Gates", "microsoftware.com")
+dentistscom = Startup.new("Dentists Dot Com", "Dr. Toothy", "drtoothysdentaldystopia.com")
+
+gavin = VentureCapitalist.new("Gavin Moneyman", 8000000)
+jon = VentureCapitalist.new("Jon Arbuckle", 1000000001)
+lily = VentureCapitalist.new("Lily Princess", 500000000)
+
+round1 = FundingRound.new(flatiron, gavin, "angel", 1*10**7)
+round2 = FundingRound.new(microsoft, jon, "seed", 5*10**8)
+round3 = FundingRound.new(dentistscom, gavin, "series A", 6*10**5)
+round4 = FundingRound.new(microsoft, lily, "Series B", 5.75)
+dentistscom.sign_contract(gavin, "series C", 5050501.10)
+gavin.offer_contract(dentistscom, "Series D", 3.50)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
