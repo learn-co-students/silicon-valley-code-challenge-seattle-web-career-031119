b6b54d53123a7ff9508ec4e5d99d232c20439267
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-03-18 14:49:37 -0700

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,20 @@
+#Belongs to Startup AND VentureCapitalist
+
 class FundingRound
+
+  attr_reader :startup, :venture_capitalist, :type, :investment
+  @@all = []
+
+  def initialize(startup, venture_capitalist, type, investment )
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @investment = investment
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,66 @@
+#Has many FundingRound
+#Has many VentureCapitalist through FundingRound
+require "pry"
 class Startup
+
+  attr_reader :name, :founder, :domain
+
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def pivot(domain, name)
+    @name = name
+    @domain = domain
+  end
+
+  def sign_contact(venture_capitalist, type, amount)
+    FundingRound.new(self, venture_capitalist, type, amount)
+  end
+
+  def funding_rounds
+    FundingRound.all.select{|round| round.startup == self}
+  end
+
+  def num_funding_rounds
+    self.funding_rounds.count
+  end
+
+  def total_funds
+    investments = self.funding_rounds.map{|round| round.investment}
+    total = 0
+
+    investments.each do |investment|
+      total += investment
+    end
+
+    total
+
+  end
+
+  def investors
+    self.funding_rounds.map {|round| round.venture_capitalist}.uniq
+  end
+
+  def big_investors
+    self.investors.select{|investor| VentureCapitalist.tres_commas_club.include?(investor)}
+  end
+
+  def self.find_by_founder(founder)
+    self.all.select {|startup| startup.founder == founder}
+  end
+
+  def self.domains
+    self.all.map {|startup| startup.domain}.uniq
+  end
+
+  def self.all
+    @@all
+  end
 
 end
